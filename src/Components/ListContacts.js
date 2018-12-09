9fe4c1e3421aaf28d708d9f44f74281bd318272a
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-12-09 11:16:27 -0200

    Adicionando input para busca de contatos

--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Contact from './Contact';
 
-const ListContacts = (props) => {
-  const { onDeleteContact } = props;
 
-  const contacts = props.contacts.map((contact) => (
-    <Contact key={contact.id} contact={contact} onDeleteContact={onDeleteContact} />
-  ));
-  
-  return (
-    <ol className='contact-list'>
-      {contacts}
-    </ol>
-  );
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery(newQuery) {
+    this.setState({
+      query: newQuery
+    });
+  }
+
+  render() {
+    const { onDeleteContact } = this.props;
+
+    const contacts = this.props.contacts.map((contact) => (
+      <Contact key={contact.id} contact={contact} onDeleteContact={onDeleteContact} />
+    ));
+
+    return (
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search Contacts'
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+
+        <ol className='contact-list'>
+          {contacts}
+        </ol>
+      </div>
+    );
+  }
 }
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-}
 
 export default ListContacts;
