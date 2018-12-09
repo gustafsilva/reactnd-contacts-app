440129159560862318886b53142cb8686c529957
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-12-09 18:54:32 -0200

    Updates

---
+++ precache-manifest.18b234ed471118e6d7a1751d25935ef4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/reactnd-contacts-app/static/media/search.0fc053ba.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/reactnd-contacts-app/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/reactnd-contacts-app/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/reactnd-contacts-app/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/reactnd-contacts-app/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "d9d45650fa0acdf4b068",
+    "url": "/reactnd-contacts-app/static/js/runtime~main.d9d45650.js"
+  },
+  {
+    "revision": "a9bdb09949b402437358",
+    "url": "/reactnd-contacts-app/static/js/main.a9bdb099.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/reactnd-contacts-app/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "a9bdb09949b402437358",
+    "url": "/reactnd-contacts-app/static/css/main.38366d52.chunk.css"
+  },
+  {
+    "revision": "2a9e38fac27fcedf9db882b0568d4ad4",
+    "url": "/reactnd-contacts-app/index.html"
+  }
+];
